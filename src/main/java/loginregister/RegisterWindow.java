e3670a0ce826c4cac1639b17c08408090520d25b
Kerim <kbukvic861@gmail.com>
2026-01-05 22:11:25 +0100

    U login i register prozor ubačen meni za izbor boje/izgleda aplikacije

--- src/main/java/loginregister/RegisterWindow.java
+++ src/main/java/loginregister/RegisterWindow.java
@@ -21,7 +21,7 @@ import java.awt.*;
         }
 
         private void initComponents() {
-            JPanel formPanel = new JPanel(new GridLayout(2, 2, 5, 5));
+            JPanel formPanel = new JPanel(new GridLayout(3, 2, 5, 5));
             formPanel.setBorder(BorderFactory.createTitledBorder("Kreiraj novi profil"));
 
             usernameField = new JTextField(20);
@@ -31,6 +31,49 @@ import java.awt.*;
             formPanel.add(usernameField);
             formPanel.add(new JLabel("Lozinka :"));
             formPanel.add(passwordField);
+
+            String[] theme = {"Plava", "Zelena", "Roza", "Narandžasta", "Dark", "Cyberpunk"};
+            JComboBox<String> themeCombo = new JComboBox<>(theme);
+
+            formPanel.add(new JLabel("Izbor boje : "));
+            formPanel.add(themeCombo);
+
+            themeCombo.addActionListener(e -> {
+                String color = (String) themeCombo.getSelectedItem();
+
+                if (color.equals("Dark")) {
+                    formPanel.setBackground(Color.DARK_GRAY);
+                    for (Component c : formPanel.getComponents()) {
+                        c.setForeground(Color.WHITE);
+                    }
+                } else if (color.equals("Cyberpunk")) {
+                    formPanel.setBackground(new Color(20, 20, 40));
+                    for (Component c : formPanel.getComponents()) {
+                        c.setForeground(Color.MAGENTA);
+                    }
+                } else if (color.equals("Plava")) {
+                    formPanel.setBackground(Color.BLUE);
+                    for (Component c : formPanel.getComponents()) {
+                        c.setForeground(Color.BLACK);
+                    }
+                } else if (color.equals("Zelena")) {
+                    formPanel.setBackground(Color.GREEN);
+                    for (Component c : formPanel.getComponents()) {
+                        c.setForeground(Color.BLACK);
+                    }
+                } else if (color.equals("Roza")) {
+                    formPanel.setBackground(Color.PINK);
+                    for (Component c : formPanel.getComponents()) {
+                        c.setForeground(Color.BLACK);
+                    }
+                } else if (color.equals("Narandžasta")) {
+                    formPanel.setBackground(Color.ORANGE);
+                    for (Component c : formPanel.getComponents()) {
+                        c.setForeground(Color.BLACK);
+                    }
+                }
+                formPanel.repaint();
+            });
 
             add(formPanel, BorderLayout.CENTER);
 
